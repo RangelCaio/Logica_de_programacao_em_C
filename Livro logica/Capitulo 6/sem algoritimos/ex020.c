68cb254aacfbea266f183b7df5e99c858b186cf2
RangelCaio <166851980+RangelCaio@users.noreply.github.com>
2024-05-04 14:01:26 -0300

    ex020

--- Livro logica/Capitulo 6/sem algoritimos/ex020.c
+++ Livro logica/Capitulo 6/sem algoritimos/ex020.c
@@ -2,25 +2,31 @@
 
 int main(){
 
-    int vetor[10], pos[10] = {0}, i, contador_pos = 0;
+    int vetor1[10], vetor2[10], i;
 
     for(i = 0; i < 10; i++){
         printf("Digite um numero para vetor[%d]: ", i);
-        scanf("%d", &vetor[i]);
+        scanf("%d", &vetor1[i]);
     }
 
     for(i = 0; i < 10; i++){
-        if(vetor[i] >= 0){
-            pos[contador_pos] += vetor[i];
-            contador_pos++;
+        vetor2[i] = vetor1[i];
+        if(vetor2[i] == 0){
+            vetor2[i] = 1;
         }
     }
 
     printf("\n");
 
-    printf("#Vetor numeros positivos: ");
-    for(i = 0; i < contador_pos; i++){
-        printf("[%d]", pos[i]);
+    printf("#############Vetor inicinal: ");
+    for(i = 0; i < 10; i++){
+        printf("[%d]", vetor1[i]);
+    }
+    printf("\n");
+
+    printf("\n#Vetor substituindo 0 por 1: ");
+    for(i = 0; i < 10; i++){
+        printf("[%d]", vetor2[i]);
     }
 
 
