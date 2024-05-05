36d6b5ff4bc5e2a29f147b68d2955468cda625d5
RangelCaio <166851980+RangelCaio@users.noreply.github.com>
2024-05-05 13:43:02 -0300

    att007

--- Livro logica/Capitulo 7/sem algoritimos/ex007.c
+++ Livro logica/Capitulo 7/sem algoritimos/ex007.c
@@ -2,33 +2,41 @@
 
 int main() {
 
-    int matriz1[4][6], matriz2[6][4], i, j;
+
+    int matriz1[4][6], i, j, matriz2[6][4], soma_linhas[4] = {0}, soma_colunas[4] = {0};
 
     printf("#matriz1#\n");
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 6; j++){
-            printf("Digite um numero para: matriz[%d][%d]: ", i, j);
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 6; j++) {
+            printf("Digite um número para matriz[%d][%d]: ", i, j);
             scanf("%d", &matriz1[i][j]);
         }
     }
 
     printf("#matriz2#\n");
-    for(i = 0; i < 6; i++){
-        for(j = 0; j < 4; j++){
-            printf("Digite um numero para: matriz[%d][%d]: ", i, j);
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 4; j++) {
+            printf("Digite um número para matriz[%d][%d]: ", i, j);
             scanf("%d", &matriz2[i][j]);
         }
     }
 
-    printf("\nMatriz resultante da soma de cada elemento pela soma da coluna correspondente:\n\n");
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 6; j++){
-            matriz1[i][j] += matriz2[i][j];
-            printf("%d   ", matriz1[i][j]);
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 6; j++) {
+            soma_linhas[i] += matriz1[i][j];
         }
-        printf("\n\n");
     }
 
+    for (j = 0; j < 4; j++) {
+        for (i = 0; i < 6; i++) {
+            soma_colunas[j] += matriz2[i][j];
+        }
+    }
+
+    printf("\nSoma das linhas de M com as colunas de N:\n");
+    for (j = 0; j < 4; j++) {
+        printf("Linha %d de M com coluna %d de N: %d\n", j, j, soma_linhas[j] + soma_colunas[j]);
+    }
 
     return 0;
 }
