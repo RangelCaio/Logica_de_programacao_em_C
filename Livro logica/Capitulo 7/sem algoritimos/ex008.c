23cfaf8065e2e7dd57c621e64416fd49cb35ea09
RangelCaio <166851980+RangelCaio@users.noreply.github.com>
2024-05-05 13:54:43 -0300

    att

---
+++ Livro logica/Capitulo 7/sem algoritimos/ex008.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+
+    int matriz1[3][8], i, j, matriz2[3][8], matrizmult[3][8], matrizsub[3][8];
+
+    printf("#matriz1#\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 8; j++) {
+            printf("Digite um numero para matriz[%d][%d]: ", i, j);
+            scanf("%d", &matriz1[i][j]);
+        }
+    }
+
+    printf("#matriz2#\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 8; j++) {
+            printf("Digite um numero para matriz[%d][%d]: ", i, j);
+            scanf("%d", &matriz2[i][j]);
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 8; j++) {
+            matrizsub[i][j] = matriz1[i][j] - matriz2[i][j];
+        }
+    }
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 8; j++) {
+            matrizmult[i][j] = matriz1[i][j] * matriz2[i][j];
+        }
+    }
+
+    printf("\nResultado da subtracao da matriz1 pela matriz2\n\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 8; j++) {
+            printf("%d  ", matrizsub[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nResultado da multiplicacao da matriz1 pela matriz2\n\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 8; j++) {
+            printf("%d  ", matrizmult[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
